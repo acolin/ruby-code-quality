4fee23b80fb19b04d827ea043d43fe0c4a95d3df
colin <acolin@incaztech.com>
2016-06-15 00:37:49 -0500

    exercises day 2

---
+++ ejercicios/dia2/selectors.js
@@ -0,0 +1,34 @@
+/**
+ * Seleccionar los elementos con los sig. IDs
+ * ticket_source
+ * ticket_full_name
+ * cart-total
+ **/
+
+/**
+ * Seleccionar los elementos con las sig. clases
+ * numbered-list
+ * flush--top
+ * help-ticket-question
+ **/
+
+/**
+ * Seleccionar todos los elementos con los sig. tipos
+ * p
+ * div
+ * label
+ **/
+
+/**
+ * Seleccionar todos los elementos
+ * p dentro de un div con clase help-ticket-question
+ **/
+
+/**
+ * Seleccionar todos los hijos inmediatos tipo option del select
+ * con ID ticket_issue_type
+ **/
+
+/**
+ * Seleccionar el tag option selected del select con ID ticket_issue_type
+ **/
