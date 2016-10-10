d3ba9d24095f63b57ebfc34b7c9c5150705d5138
Arnoldo Rodriguez Colin <rodriguezcolin@gmail.com>
2016-10-10 18:00:09 -0500

    Update functions_exercises.rb

--- ejercicios/dia1/functions_exercises.rb
+++ ejercicios/dia1/functions_exercises.rb
@@ -1,1 +1,25 @@
+# Problem 1: Create the select tag function(functions) from rails form helpers
+# Method call:
+# select_tag('my tag', [['title','value']], html={id: 'hello'})
+#
+# Output:
+# <select id='hello'>
+#  <option value='value'>title</option>
+# </select>
 
+
+
+# Problem 2: Create a user including the following attributes
+# Assume that a User, Location & Preference Models exists
+# Method call: create_user({
+#  email: 'juan@gmail.com',
+#  fullname: 'Juan Garcia',
+#  location: {
+#    address1: 'via corso 101',
+#    city: 'san pedro garza garcia'
+#  },
+#  preferences: {
+#    favorite_color: 'blue',
+#    has_a_pet: true
+#  }
+# })
