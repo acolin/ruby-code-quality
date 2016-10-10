4e22088fd042158af8d6d180edffa679d0573f1a
Arnoldo Rodriguez Colin <rodriguezcolin@gmail.com>
2016-10-10 18:54:12 -0500

    Create srp_exercises.rb

---
+++ ejercicios/dia2/srp_exercises.rb
@@ -0,0 +1,3 @@
+# Create a service that will reset a password
+# Create a service that will launch a phone call via twillio
+# Create a module for calculating the order total with adjustments (discount, shipping cost)
