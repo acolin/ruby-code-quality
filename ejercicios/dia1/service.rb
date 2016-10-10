94432508d20405f2afeddc48064f3ad8cd9d3eea
Arnoldo Rodriguez Colin <rodriguezcolin@gmail.com>
2016-10-10 18:21:37 -0500

    Update service.rb

--- ejercicios/dia1/service.rb
+++ ejercicios/dia1/service.rb
@@ -1,1 +1,36 @@
-
+class CreateAccount
+  include ActiveModel::Model
+  attr_accessor :email, :fullname, :address1, :city
+  attr_accessor :user, :location
+  
+  validates :email, :fullname, :address1, :city, presence: true
+  
+  def call
+    return false unless valid?
+    
+    begin
+      ActiveRecord::Base.transaction do
+        create_user
+        create_location
+      end
+      send_welcome_notification
+      true
+    rescue => e
+      errors.add(:base, 'There was a problem with the db, please try again later.')
+    end
+  end
+  
+  private
+  
+  def create_user
+    @user ||= User.create(email: email, fullname: fullname)
+  end
+  
+  def create_location
+    @location ||= Location.create(address1: address1, city: city)
+  end
+  
+  def send_welcome_notification
+    UserMailer.welcome_email(user).deliver_later
+  end
+end
