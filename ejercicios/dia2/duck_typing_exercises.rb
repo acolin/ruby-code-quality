955129f8cd42450294a7c2dbed51229bb726c07c
Arnoldo Rodriguez Colin <rodriguezcolin@gmail.com>
2016-10-10 19:00:18 -0500

    Create duck_typing_exercises.rb

---
+++ ejercicios/dia2/duck_typing_exercises.rb
@@ -0,0 +1,3 @@
+# create a multi report module (text, html)
+# create a multi geocoder, will geocode an address from at least 2 services such as googlemaps & heremaps
+# create a multi discount calculator having a % percentage, fixed & 2x1 calculators
