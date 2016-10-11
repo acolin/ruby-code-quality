22e8aee3a61c5afddf820f0c07d4fcca67bf3b5b
Arnoldo Rodriguez Colin <rodriguezcolin@gmail.com>
2016-10-10 19:01:43 -0500

    Update roles_exercises.rb

--- ejercicios/dia2/roles_exercises.rb
+++ ejercicios/dia2/roles_exercises.rb
@@ -1,2 +1,2 @@
-# Create a role for generating unique identifiers
-# Create a role to create a taggable object
+# Create a role for generating unique identifiers i.e. order->number 10AxY6dsrfc713
+# Create a role to create a taggable object i.e. any model can have multiple tags user -> rock, pop
